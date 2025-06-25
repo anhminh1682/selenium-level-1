fbed69805224e7a08976aec7e6e9a0db3809b995
Anh Minh <nguyenanhminh168202@gmail.com>
2025-06-25 21:49:46 +0700

    Completed TC16

--- src/main/java/com/railway/pages/MyTicketPage.java
+++ src/main/java/com/railway/pages/MyTicketPage.java
@@ -1,6 +1,52 @@
 package com.railway.pages;
 
+import com.railway.driver.DriverManager;
+import com.railway.utilities.Helpers;
+import com.railway.utilities.Ticket;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
 public class MyTicketPage extends BasePage {
+    private final String rowOfTicketXpath = "//table[contains(@class, 'MyTable')]//tr[td[text()='%s'] and td[text()='%s'] and td[text()='%s'] and td[text()='%s'] and td[text()='%s']]//input[@value='Cancel']";
 
+    private List<WebElement> getListRowOfTicket(Ticket ticket) {
+        return DriverManager.getDriver().findElements(
+                By.xpath(
+                        String.format(
+                                rowOfTicketXpath,
+                                ticket.getDepartStation(),
+                                ticket.getArriveStation(),
+                                ticket.getSeatType(),
+                                ticket.getDate(),
+                                ticket.getTicketAmount()
+                        )
+                )
+        );
+    }
 
+    public int countOfRowTicket(Ticket ticket) {
+        return getListRowOfTicket(ticket).size();
+    }
+
+    public void cancelATicket(Ticket ticket) {
+        WebElement element = webElement(By.xpath(String.format(
+                rowOfTicketXpath,
+                ticket.getDepartStation(),
+                ticket.getArriveStation(),
+                ticket.getSeatType(),
+                ticket.getDate(),
+                ticket.getTicketAmount()
+        )));
+        Helpers.scrollToElement(element);
+        element.click();
+
+        // Click pop up
+        Helpers.waitAlert(10);
+        Alert alert = DriverManager.getDriver().switchTo().alert();
+
+        alert.accept();
+    }
 }
--- src/test/java/com/tests/myticket/TC16.java
+++ src/test/java/com/tests/myticket/TC16.java
@@ -6,6 +6,7 @@ import com.railway.utilities.Ticket;
 import com.railway.utilities.enums.ProvincesEnums;
 import com.railway.utilities.enums.SeatTypeEnums;
 import com.tests.base.TestBase;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.time.LocalDateTime;
@@ -15,9 +16,9 @@ public class TC16 extends TestBase {
     public void userCanCancelATicket (){
         HomePage homePage = new HomePage();
         LoginPage loginPage = new LoginPage();
-        TimetablePage timetablePage = new TimetablePage();
         BookTicketPage bookTicketPage = new BookTicketPage();
         BookTicketSuccessfulPage bookTicketSuccessfulPage = new BookTicketSuccessfulPage();
+        MyTicketPage myTicketPage = new MyTicketPage();
 
         // 2. Login with a valid account
         homePage.clickOnTab(Constants.TabMenu.LOGIN_TAB);
@@ -39,11 +40,17 @@ public class TC16 extends TestBase {
         // 4. Click on "My ticket" tab
         bookTicketSuccessfulPage.clickOnTab(Constants.TabMenu.MY_TICKET_TAB);
 
+        // Count the amount of tickets before cancel
+        int oldCount = myTicketPage.countOfRowTicket(ticket);
+
         // 5. Click on "Cancel" button of ticket which user want to cancel.
-
-
         // 6. Click on "OK" button on Confirmation message "Are you sure?"
+        myTicketPage.cancelATicket(ticket);
+
+        // Count the amount of tickets after cancel
+        int newCount = myTicketPage.countOfRowTicket(ticket);
 
         // The canceled ticket is disappeared.
+        Assert.assertEquals(newCount, oldCount - 1);
     }
 }
