0291bd965aa89ccde66053fd1e13d13c598c78eb
Anh Minh <nguyenanhminh168202@gmail.com>
2025-06-26 09:40:42 +0700

    Screenshot when test case failed

--- src/test/java/com/tests/ultilities/AppListener.java
+++ src/test/java/com/tests/ultilities/AppListener.java
@@ -3,26 +3,44 @@ package com.tests.ultilities;
 import com.railway.driver.DriverManager;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.bidi.browsingcontext.BrowsingContext;
 import org.openqa.selenium.io.FileHandler;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 public class AppListener implements ITestListener {
     @Override
     public void onTestSuccess(ITestResult result) {
-        System.out.println("Test passed!");
+        System.out.println("Test case passed!");
     }
 
     @Override
     public void onTestFailure(ITestResult result) {
         File srcFile = ((TakesScreenshot) DriverManager.getDriver()).getScreenshotAs(OutputType.FILE);
-        File destFile = new File("D:\\AGEST\\SeleniumExercise\\SeleniumLevel1\\test-images\\failed-image");
+
+        String testName = result.getMethod().getMethodName();
+        String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("ddMMyyyy_HHmmss"));
+        String fileName = testName + "_" + timestamp + ".png";
+
+        String baseDir = System.getProperty("user.dir");
+        String failedDir = "test-images/" + result.getTestClass().getName();
+
+        File destFile = new File(baseDir + File.separator + failedDir + File.separator + fileName);
 
         try {
-            FileHandler.copy(srcFile, destFile);
+            boolean dirExist = destFile.getParentFile().mkdirs();
+
+            if(dirExist) {
+                FileHandler.copy(srcFile, destFile);
+                System.out.println("📸 screenshot saved to: " + destFile.getAbsolutePath());
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
